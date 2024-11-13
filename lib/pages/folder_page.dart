77c9ef65db07da9118db43e0d8e7e863848ca733
Prasoon Gautam <109397711+gtmpras@users.noreply.github.com>
2024-11-13 07:34:06 +0545

    pdf storing collection

--- lib/pages/folder_page.dart
+++ lib/pages/folder_page.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 class FoldersPage extends StatefulWidget {
@@ -6,37 +11,82 @@ class FoldersPage extends StatefulWidget {
 }
 
 class _FoldersPageState extends State<FoldersPage> {
+  //instance for firebase firestore
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+
+  //for uploading pdf part
+  Future<String> uploadPdf(String fileName, File file) async {
+    final refrence = FirebaseStorage.instance.ref().child(
+        "pdfs/$fileName.pdf"); //defining the path where the file is stored
+
+    final uploadTask = refrence.putFile(file);
+
+    await uploadTask.whenComplete(() {});
+
+    final downloadLink = await refrence.getDownloadURL();
+
+    return downloadLink;
+  }
+
+  //for file picking
+  void pickFile() async {
+    final pickedFile = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf'],
+    );
+
+    if (pickedFile != null) {
+      String fileName = pickedFile.files[0].name;
+      File file = File(pickedFile.files[0].path!);
+      uploadPdf(fileName, file);
+
+      final downloadLink = await uploadPdf(fileName, file);
+
+      //for storing pdf into the collection
+      await _firebaseFirestore
+          .collection("pdfs")
+          .add({"name": fileName, "url": downloadLink});
+      print("Pdf uploaded successfully");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return 
-    Scaffold(
-      appBar: AppBar(title: Text("Pdf Files"),),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pdf Files"),
+      ),
       body: GridView.builder(
-        itemCount: 10,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-         itemBuilder: (context,index){
-          return Padding(padding:const EdgeInsets.all(8.0),
-          child: InkWell(
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all()
+          itemCount: 10,
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: InkWell(
+                child: Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset(
+                        "images/pdf_logo.png",
+                        height: 120,
+                        width: 100,
+                      ),
+                      Text(
+                        "Pdf Name",
+                        style: TextStyle(fontSize: 18),
+                      )
+                    ],
+                  ),
+                ),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Image.asset("images/pdf_logo.png",
-                  height: 120,
-                  width: 100,),
-                  Text("Pdf Name",
-                  style: TextStyle(
-                    fontSize: 18
-                  ),)
-                ],
-              ),
-            ),
-          ),);
-          
-         }),
-        );
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.upload_file),
+           onPressed: pickFile),
+    );
   }
 }
